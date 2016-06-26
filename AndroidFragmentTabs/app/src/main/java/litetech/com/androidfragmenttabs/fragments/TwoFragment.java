83e1c7cf77c5076d187735767f868ca684b2595e
Edwin Lambregts <edwin.lambregts0@gmail.com>
2016-06-26 16:59:28 +0200

    Finish

--- AndroidFragmentTabs/app/src/main/java/litetech/com/androidfragmenttabs/fragments/TwoFragment.java
+++ AndroidFragmentTabs/app/src/main/java/litetech/com/androidfragmenttabs/fragments/TwoFragment.java
@@ -11,18 +11,19 @@ import litetech.com.androidfragmenttabs.R;
 /**
  * Created by lol_e on 26-6-2016.
  */
-public class OneFragment extends Fragment {
+public class TwoFragment extends Fragment {
 
-    public OneFragment() { }
+    public TwoFragment() { }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_one, container, false);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_two, container, false);
     }
 
 }
